a956742b0c7f59e14eb6f36be84dfc469cc5caf5
Raihan <raihan9419@gmail.com>
2020-06-25 00:11:27 +0600

    customize admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -6,4 +6,14 @@ from .models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish','status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = { 'slug': ('title',) }
+    date_hierarchy = 'publish'
+    raw_id_fields = ('author',)
+    ordering = ('status','publish')
